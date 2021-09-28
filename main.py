1e7c28a5ea815c315c47690cd39999af8c0732f0
Your Name <827511@mystma.org>
2021-09-28 14:35:46 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-#WRITE YOUR CODE IN THIS FILE
+def password(password):
+    if password == "Knights19":
+        return "ACCESS GRANTED"
+    else:
+        return "ACCESS DENIED"
+
+print(password("Knights19"))
+
